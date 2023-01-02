ccfd1d9d3d8474531f6834f782f80fc11c696e35
Noor Mohammed <noormohammedknr2@gmail.com>
2023-01-02 23:13:43 +0530

    Updated levels of sub's

--- client/src/Components/CategoryComp/Category.js
+++ client/src/Components/CategoryComp/Category.js
@@ -10,9 +10,19 @@ const Category = () => {
     ParentCategorId: 0,
     items: [],
   });
+
+  const [categories1, setCategories1] = useState({
+    catgoryID1: 0,
+    categoryName1: "",
+    numberOfPeople1: "",
+    ParentCategorId1: 0,
+    items1: [],
+  });
   const [allCategories, setAllCategories] = useState([]);
+  const [allCategories1, setAllCategories1] = useState([]);
 
   const [show, setShow] = useState(false);
+
   const itemChangeHandler = (e) => {
     setCategories({ ...categories, categoryName: e.target.value });
   };
@@ -20,11 +30,24 @@ const Category = () => {
   const personsChangeHandler = (e) => {
     setCategories({ ...categories, numberOfPeople: e.target.value });
   };
+  const item1ChangeHandler = (e) => {
+    setCategories1({ ...categories1, categoryName1: e.target.value });
+  };
 
+  const persons1ChangeHandler = (e) => {
+    setCategories1({ ...categories1, numberOfPeople1: e.target.value });
+  };
   const keyDownHandler = (e) => {
     if (e.key === "Enter") {
       setAllCategories([...allCategories, categories]);
       console.log(allCategories);
+      setShow(true);
+    }
+  };
+  const keyDownHandler1 = (e) => {
+    if (e.key === "Enter") {
+      setAllCategories1([...allCategories1, categories1]);
+      console.log(allCategories1);
       setShow(true);
     }
   };
@@ -33,7 +56,32 @@ const Category = () => {
       <div className="header">
         <h1 className="header__title">Menu</h1>
         <div className="menupage">
-          {show && <DisplayCategory allCategories={allCategories} />}
+          {show && (
+            <DisplayCategory
+              allCategories={allCategories}
+              allCategories1={allCategories1}
+            />
+          )}
+
+          {show && (
+            <div className="menupage-list" onKeyDown={keyDownHandler1}>
+              <input
+                type="text"
+                className="menupage_list__items"
+                placeholder="Sub Category name"
+                onChange={item1ChangeHandler}
+                required
+                autoFocus
+              />
+              <input
+                type="number"
+                className="menupage_list__items"
+                placeholder="Number of People"
+                onChange={persons1ChangeHandler}
+                required
+              />
+            </div>
+          )}
           <div className="menupage-list" onKeyDown={keyDownHandler}>
             <input
               type="text"
